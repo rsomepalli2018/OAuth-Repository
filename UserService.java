63b3d23b3e6c524d32ad5f44f89b5caa56e9de5d
rsomepalli2018 <rajesh.java1984@gmail.com>
2017-07-01 14:46:54 -0500

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,28 @@
+package com.oauth2.service;
+
+import java.util.List;
+
+import com.oauth2.model.User;
+
+
+
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
